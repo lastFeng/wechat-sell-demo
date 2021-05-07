4012769525d59187b9f6f78465a55bfcccf17794
lastFeng <563805728@qq.com>
2021-05-07 12:26:57 +0800

    Refactor

--- src/main/java/com/example/wechatselldemo/controller/BuyerProductController.java
+++ src/main/java/com/example/wechatselldemo/controller/BuyerProductController.java
@@ -2,15 +2,14 @@ package com.example.wechatselldemo.controller;
 
 import com.example.wechatselldemo.domain.ProductCategory;
 import com.example.wechatselldemo.domain.ProductInfo;
-import com.example.wechatselldemo.domain.vo.ProductInfoVo;
-import com.example.wechatselldemo.domain.vo.ProductVo;
+import com.example.wechatselldemo.domain.vo.product.ProductInfoVo;
+import com.example.wechatselldemo.domain.vo.product.ProductVo;
 import com.example.wechatselldemo.domain.vo.ResultVo;
 import com.example.wechatselldemo.service.ProductCategoryService;
 import com.example.wechatselldemo.service.ProductInfoService;
 import com.example.wechatselldemo.utils.ResultVoUtils;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +17,6 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
-import java.util.stream.Collectors;
 
 import static java.util.stream.Collectors.*;
 
--- src/main/java/com/example/wechatselldemo/domain/vo/product/ProductInfoVo.java
+++ src/main/java/com/example/wechatselldemo/domain/vo/product/ProductInfoVo.java
@@ -1,4 +1,4 @@
-package com.example.wechatselldemo.domain.vo;
+package com.example.wechatselldemo.domain.vo.product;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
